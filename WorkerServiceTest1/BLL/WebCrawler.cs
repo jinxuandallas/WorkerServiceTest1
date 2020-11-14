b25cb4a44e3be49193e7d83eae48e3760eb20f06
Dallas <dallas2k@163.com>
2020-11-14 10:45:46 +0800

    ++

---
+++ WorkerServiceTest1/BLL/WebCrawler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace WorkerServiceTest1.BLL
+{
+    public interface IWebCrawler
+    {
+        void SaveHtmlTxt(string url);
+    }
+
+    public class WebCrawler : IWebCrawler
+    {
+        public void SaveHtmlTxt(string url)
+        {
+            string strHTML = "";
+            WebClient myWebClient = new WebClient();
+            Stream myStream = myWebClient.OpenRead(url);
+            StreamReader sr = new StreamReader(myStream, Encoding.Default);//注意编码
+            strHTML = sr.ReadToEnd();
+            myStream.Close();
+
+            File.WriteAllText(@"c:\example.txt", strHTML);
+            //return strHTML;
+        }
+    }
+}
--- WorkerServiceTest1/Worker.cs
+++ WorkerServiceTest1/Worker.cs
@@ -13,18 +13,22 @@ namespace WorkerServiceTest1
     {
         private readonly ILogger<Worker> _logger;
         private IAddDatabase _iad;
+        private IWebCrawler _iwc;
 
-        public Worker(ILogger<Worker> logger,IAddDatabase iad)
+        public Worker(ILogger<Worker> logger,IAddDatabase iad,IWebCrawler iwc)
         {
             _logger = logger;
             _iad = iad;
+            _iwc = iwc;
         }
 
         public override async Task StartAsync(CancellationToken cancellationToken)
         {
             //_iad.dbOpen();
-            _logger.LogInformation("database open");
+            //_logger.LogInformation("database open");
             //_iad.dbClose();
+            _iwc.SaveHtmlTxt(@"http://www.baidu.com");
+            _logger.LogInformation("OK!");
             await base.StartAsync(cancellationToken);
         }
 
